efe1b27f5e852e76923c6ecc1cdeeee4f08bdd23
jgbrioso <briosojob@gmail.com>
2019-05-04 15:27:23 +0800

    Update add_cars.php

--- finalproj/car_rental/admin/add_cars.php
+++ finalproj/car_rental/admin/add_cars.php
@@ -147,9 +147,9 @@
 
 <div id="footer">
 	<div class="shell">
-		<span class="left">&copy; <?php echo date("Y");?> - Sonko Rescue Team</span>
+		<span class="left">&copy; <?php echo date("Y");?> ---</span>
 		<span class="right">
-			Design by Consi</a>
+			JGB</a>
 		</span>
 	</div>
 </div>
